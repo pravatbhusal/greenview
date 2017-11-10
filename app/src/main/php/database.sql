047f6c0386df4d490b570ddfde2bb13eb860473f
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-11-09 18:58:08 -0600

    modify the database for recycable items

--- app/src/main/php/database.sql
+++ app/src/main/php/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 05, 2017 at 05:33 PM
+-- Generation Time: Nov 10, 2017 at 12:55 AM
 -- Server version: 10.1.20-MariaDB
 -- PHP Version: 7.0.8
 
@@ -23,46 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recyclable_items`
---
-
-CREATE TABLE `recyclable_items` (
-  `id` int(11) NOT NULL,
-  `item` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `recyclable_items`
---
-
-INSERT INTO `recyclable_items` (`id`, `item`) VALUES
-(63, 'aluminium'),
-(64, 'tin'),
-(65, 'can'),
-(66, 'cardboard'),
-(67, 'envelope'),
-(68, 'paper'),
-(69, 'magazine'),
-(70, 'mail'),
-(71, 'newspaper'),
-(72, 'book'),
-(73, 'oil'),
-(74, 'motor oil'),
-(75, 'flyer'),
-(76, 'folder'),
-(77, 'bottle'),
-(78, 'plastic'),
-(79, 'box'),
-(80, 'water'),
-(81, 'drink'),
-(82, 'jug'),
-(83, 'condiment'),
-(84, 'tub'),
-(85, 'lid');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -73,15 +33,10 @@ CREATE TABLE `users` (
   `score` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `recyclable_items`
---
-ALTER TABLE `recyclable_items`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -94,15 +49,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `recyclable_items`
---
-ALTER TABLE `recyclable_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
