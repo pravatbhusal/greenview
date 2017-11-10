4d270fcc2e0fb348142c33b54873a2b2aaedbb23
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-11-10 16:51:47 -0600

    Check if userID or Name is not empty

--- app/src/main/php/register.php
+++ app/src/main/php/register.php
@@ -5,16 +5,21 @@ include("dbconnection.php");
 $Name = $_POST["Name"];
 $userID = $_POST["userID"];
 
-        //check if the userID has already been used. if it is, then don't allow them to use it!
-	$query = "SELECT * FROM users WHERE userID='$userID'";
-	$result = mysqli_query($link, $query);
-    	if(mysqli_num_rows($result) > 0) {
-             exit;
-    	}
-
-	//this query inserts the values into the club_info table
-	$query = "INSERT INTO users (Name, userID) VALUES('$Name', '$userID')";
-	mysqli_query($link, $query);
-
+//make sure the userID or Name is not empty
+if(empty($userID) || empty($Name)) {
 	exit;
+}
+
+//check if the userID has already been used. if it is, then don't allow them to use it!
+$query = "SELECT * FROM users WHERE userID='$userID'";
+$result = mysqli_query($link, $query);
+	if(mysqli_num_rows($result) > 0) {
+		 exit;
+	}
+
+//this query inserts the values into the club_info table
+$query = "INSERT INTO users (Name, userID) VALUES('$Name', '$userID')";
+mysqli_query($link, $query);
+
+exit;
 ?>
